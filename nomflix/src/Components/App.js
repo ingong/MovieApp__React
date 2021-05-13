513f3be9abdb06fd9018ed09517b2b29120cebcc
leeinsong <insong1216@gmail.com>
2021-05-14 00:51:08 +0900

    Add: GlobalStyles

--- nomflix/src/Components/App.js
+++ nomflix/src/Components/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import Router from "./Router";
+import GlobalStyles from "../Components/GlobalStyles";
+
 export default class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     );
   }
