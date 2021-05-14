aa8f0f12a24df9cdaf143d34160b0a9566127273
leeinsong <insong1216@gmail.com>
2021-05-15 03:47:29 +0900

    Add: 컨테이너 끝

--- nomflix/src/Components/Router.js
+++ nomflix/src/Components/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Detail from "../Routes/Detail";
 import Header from "../Components/Header";
 
 // todo
@@ -16,7 +17,9 @@ export default () => {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/tv' component={TV} />
-          <Route path='/search' exact component={Search} />
+          <Route path='/search' component={Search} />
+          <Route path='/movie/:id' component={Detail} />
+          <Route path='/show/:id' component={Detail} />
           <Redirect from='*' to='/' />
         </Switch>
       </>
